84e5490949df8f074a7b5c466b68ee9e9929f0be
hsabag <haim.sabag@gmail.com>
2015-02-15 21:26:23 +0200

    fe

--- src/main/java/org/sb/persist/WorkCardRepository.java
+++
@@ -1,9 +0,0 @@
-package org.sb.persist;
-
-import org.sb.persist.data.WorkCardEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WorkCardRepository extends MongoRepository<WorkCardEntity, String> {
-
-
-}
--- src/main/java/org/sb/persist/data/WorkCardEntity.java
+++
@@ -1,8 +0,0 @@
-package org.sb.persist.data;
-
-import org.sb.data.WorkCard;
-
-public class WorkCardEntity extends Entity<WorkCard>
-{
-
-}
