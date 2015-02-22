8083ae0547a420357ac8fab542f05d2072f20217
hsabag <haim.sabag@gmail.com>
2015-02-22 22:06:21 +0200

    draft

--- fe/small/js/controllers.js
+++ fe/small/js/controllers.js
@@ -1,26 +1,39 @@
 var controllers = angular.module('F1FeederApp.controllers', []);
 
-  controllers.controller('registerCompanyController', function($scope, registerCompanyAPI) {
+  controllers.controller('registerCompanyController', function($scope, $window, companyAPI) {
 	$scope.alert = function(){
 		
-		var result = registerCompanyAPI.register($scope.company);
-		alert("registered");
+		var success = function (response) {
+		  $window.location.href= "#listComp";
+		};
+		var error = function (response) {
+		  $scope.error = response;
+		};
+		var result = companyAPI.register({},$scope.company, success, error);
+		
 	}
   });
   
   
-  controllers.controller('fetchCompanyController', function($scope, fetchCompanyAPI) {
+  controllers.controller('fetchCompanyController', function($scope, companyAPI) {
 	$scope.search = function(){
-		$scope.company = fetchCompanyAPI.get({name:$scope.name});
+		$scope.company = companyAPI.get({name:$scope.name});
 		alert(angular.toJson($scope.company));
 	}
 	
   });
   
-  controllers.controller('listCompanyController', function($scope, listCompanyAPI) {
-	$scope.search = function(){
-		$scope.companies = listCompanyAPI.get();
-	}
+  controllers.controller('listCompanyController', function($scope, companyAPI) {
+	  $scope.searchFilter = function (company) {
+        var re = new RegExp($scope.nameFilter, 'i');
+        return !$scope.nameFilter || re.test(company.name) || re.test(company.manager.firstName);
+		};
+		$scope.delete = function (name) {
+			alert("delete:" + angular.toJson(name));
+		};
+	  companyAPI.list({},function (response) {
+		  $scope.companies = response;
+    });
 	
   });
   
--- fe/small/js/services.js
+++ fe/small/js/services.js
@@ -1,10 +1,13 @@
 var smallBisServices = angular.module('F1FeederApp.services', ['ngResource']);
 
 
-smallBisServices.factory('registerCompanyAPI', ['$resource',
+smallBisServices.factory('companyAPI', ['$resource',
   function($resource, $scope){
     return $resource('http://localhost:8080/company', {}, {
-	  register: {method:'POST'}
+	  register: {method:'POST'},
+	  fetch: {method:'GET',url:'http://localhost:8080/company/:name'},
+	  list: {method:'GET', url:'http://localhost:8080/companies', isArray:true},
+	  delete: {method:'DELETE',url:'http://localhost:8080/company/:name'}
     });
   }]);
   
@@ -15,9 +18,9 @@ smallBisServices.factory('registerCompanyAPI', ['$resource',
     });
   }]);
   
-  smallBisServices.factory('listCompanyController', ['$resource',
+  smallBisServices.factory('listCompanyAPI', ['$resource',
   function($resource, $scope){
-    return $resource('http://localhost:8080/company/:name', {}, {
+    return $resource('http://localhost:8080/companies', {}, {
 	  fetch: {method:'GET'}
     });
   }]);
